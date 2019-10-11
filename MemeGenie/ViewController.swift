1d09d6a9054fd6014aa66c4a3e0853a4f9f0daec
Alexander Rivas <alexander.rivas@live.com>
2019-10-10 23:31:44 -0500

    Initial Commit

---
+++ MemeGenie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeGenie
+//
+//  Created by Team6 on 10/10/19.
+//  Copyright © 2019 Team6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
