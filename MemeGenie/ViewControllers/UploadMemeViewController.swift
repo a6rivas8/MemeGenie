aec520a29fd80a904410db58ebe962dc7d218ff3
jovani <jovaniq1@gmail.com>
2019-10-30 16:34:31 -0500

    push ViewController UploadImage

--- MemeGenie/ViewControllers/UploadMemeViewController.swift
+++ MemeGenie/ViewControllers/UploadMemeViewController.swift
@@ -8,15 +8,59 @@
 
 import UIKit
 
-class UploadMemeViewController: UIViewController {
+class UploadMemeViewController:UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let memeTags = ["Sad", "Happy","love","angry"]
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+     var originalImage = UIImage(named: "noimage")
+    
+     let picker = UIImagePickerController()
+    @IBOutlet weak var tagLabel: UILabel!
+    override func viewDidLoad(){
+    super.viewDidLoad()
+        
+    picker.delegate = self
+        
+          }
+          
+          func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+              
+            guard let selectedImage = info[.originalImage] as? UIImage else {
+                     fatalError("Expected an image, but was provided the following: \(info)")
+                 }
+                 
+                 // Set photoImageView to display the selected image
+                 imageView.image = selectedImage
+                 originalImage = selectedImage
+            
+              self.dismiss(animated: true, completion: nil)
+          }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBAction func uploadMeme(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            picker.allowsEditing=false
+            picker.sourceType = .photoLibrary
+            present(picker, animated: true, completion: nil)
+        }
     }
     
-
+    @IBAction func ChooseTags(_ sender: Any) {
+            
+            let actionSheetAlert = UIAlertController(title: "Choose a tag ", message: "", preferredStyle: .actionSheet)
+        for tags in memeTags { actionSheetAlert.addAction(UIAlertAction(title: tags, style: .default, handler: { _ in
+                       self.tagLabel.text = tags
+                   }))
+            }
+            actionSheetAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in
+                self.dismiss(animated: true, completion: nil)
+            }))
+            self.present(actionSheetAlert, animated: true, completion: nil)
+        
+    
+    }
     /*
     // MARK: - Navigation
 
