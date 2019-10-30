131220be5a23b099ef3963ddf8652ef1a9cc7e7e
Alexander Rivas <alexander.rivas@live.com>
2019-10-29 20:32:46 -0500

    worked on UI and image picker functionality

--- MemeGenie/ViewControllers/UploadMemeViewController.swift
+++ MemeGenie/ViewControllers/UploadMemeViewController.swift
@@ -8,15 +8,72 @@
 
 import UIKit
 
-class UploadMemeViewController: UIViewController {
-
+class UploadMemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let picker = UIImagePickerController()
+    
+    @IBOutlet weak var uploadImageView: UIImageView!
+    @IBOutlet weak var uploadImageCaption: UITextField!
+    @IBOutlet weak var uploadImageTags: UITextField!
+    @IBOutlet weak var uploadImageProgress: UIProgressView!
+    
+    var defaultImage = UIImage(named: "noImage.jpeg")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        picker.delegate = self
     }
     
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            return
+        }
+        
+        uploadImageView.image = selectedImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func selectPhotoForMeme(_ sender: Any) {
+        let actionAlert = UIAlertController(title: "Picker", message: "Choose one", preferredStyle: .actionSheet)
+        actionAlert.addAction(UIAlertAction(title: "Open Camera", style: .default, handler: {_ in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                self.picker.allowsEditing = false
+                self.picker.sourceType = .camera
+                self.picker.cameraCaptureMode = .photo
+                self.present(self.picker, animated: true, completion: nil)
+            } else {
+                // let user know camera is not available
+                print("Camera is not avail")
+                let alert = UIAlertController(title: "No Camera", message: "No camera was found", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: {_ in self.dismiss(animated: true, completion: nil)}))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }))
+        actionAlert.addAction(UIAlertAction(title: "Open Gallery", style: .default, handler: {_ in
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                self.picker.allowsEditing = false
+                self.picker.sourceType = .photoLibrary
+                self.present(self.picker, animated: true, completion: nil)
+            } else {
+                // could not present gallery
+                print("Gallery is not avail")
+                let alert = UIAlertController(title: "No Gallery", message: "No gallery was found", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: {_ in self.dismiss(animated: true, completion: nil)}))
+                self.present(alert, animated: true, completion: nil)
+            }
+            
+        }))
+        actionAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(actionAlert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func uploadMeme(_ sender: Any) {
+        
+    }
+    
     /*
     // MARK: - Navigation
 
