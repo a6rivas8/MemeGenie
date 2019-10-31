69ed9571ef2ed7ce4520bbab304940cb0bdffb4f
jovani <jovaniq1@gmail.com>
2019-10-31 17:38:02 -0500

    connected to the db

--- MemeGenie/ViewControllers/UploadMemeViewController.swift
+++ MemeGenie/ViewControllers/UploadMemeViewController.swift
@@ -8,17 +8,20 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class UploadMemeViewController:UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     let memeTags = ["Sad", "Happy","love","angry"]
-    
+
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var progressView: UIProgressView!
-           
+    let db = Firestore.firestore()
+    let picker = UIImagePickerController()
+
+     var defaultImage = UIImage(named: "noimage")
+     var tag = ""
+     
     
-     var originalImage = UIImage(named: "noimage")
-    
-     let picker = UIImagePickerController()
     @IBOutlet weak var tagLabel: UILabel!
     override func viewDidLoad(){
     super.viewDidLoad()
@@ -27,21 +30,21 @@ class UploadMemeViewController:UIViewController, UIImagePickerControllerDelegate
         
           }
           
-          func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+         
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+                 fatalError("Expected an image, but was provided the following: \(info)")
+             }
              
-            guard let selectedImage = info[.originalImage] as? UIImage else {
-                     fatalError("Expected an image, but was provided the following: \(info)")
-                 }
-                 
-                 // Set photoImageView to display the selected image
-                 imageView.image = selectedImage
-                 originalImage = selectedImage
-            
-              self.dismiss(animated: true, completion: nil)
-          }
+             // Set photoImageView to display the selected image
+             imageView.image = selectedImage
+             defaultImage = selectedImage
+        
+          self.dismiss(animated: true, completion: nil)
+      }
 
     
-    @IBAction func uploadMeme(_ sender: Any) {
+    @IBAction func addMeme(_ sender: Any) {
         progressView.isHidden = true
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             picker.allowsEditing=false
@@ -51,10 +54,13 @@ class UploadMemeViewController:UIViewController, UIImagePickerControllerDelegate
     }
     
     @IBAction func ChooseTags(_ sender: Any) {
+ 
             
             let actionSheetAlert = UIAlertController(title: "Choose a tag ", message: "", preferredStyle: .actionSheet)
         for tags in memeTags { actionSheetAlert.addAction(UIAlertAction(title: tags, style: .default, handler: { _ in
-                       self.tagLabel.text = tags
+            self.tagLabel.text = tags
+            self.tag += tags
+       
                    }))
             }
             actionSheetAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in
@@ -66,10 +72,13 @@ class UploadMemeViewController:UIViewController, UIImagePickerControllerDelegate
     }
     
     @IBAction func uploadImg(_ sender: Any) {
+        if imageView.image == defaultImage {
+        
         progressView.isHidden = false
-        let ramdomID = UUID.init().uuidString
-        let uploadRef = Storage.storage().reference(withPath: "memes/\(ramdomID).jpg")
-        guard let imageData = imageView.image?.jpegData(compressionQuality: 0.75) else { return }
+        let randomID = UUID.init().uuidString
+        let uploadRef = Storage.storage().reference(withPath: "memes/\(randomID).jpg")
+        guard let imageData = imageView.image?.jpegData(compressionQuality: 0.075) else { return }
+            
         let uploadMetadata = StorageMetadata.init()
         uploadMetadata.contentType = "image/jpeg"
         
@@ -79,22 +88,45 @@ class UploadMemeViewController:UIViewController, UIImagePickerControllerDelegate
                 return
             }
             print(" completion and this is what we got back:  \(String(describing: downloadMetadata))")
-    }
-        taskReference.observe(.progress) { [weak self] (Snapshot) in
-            guard let pctThere = Snapshot.progress?.fractionCompleted else { return }
-            print("you are \(pctThere) complete")
-            self?.progressView.progress = Float(pctThere)
-        }
-    
-    /*
-    // MARK: - Navigation
+   
+        
+            // Create meme reference in Firestore database
+                       self.db.collection("memes").document(randomID).setData([
+                        "tags": self.tag,
+                           "date_uploaded": downloadMetadata?.timeCreated! ?? Timestamp(date: Date()),
+                           "likes": 0,
+                           "passes": 0,
+                           "rank": 0,
+                       ]) { err in
+                           if let err = err {
+                               print("Error writing meme document: \(err.localizedDescription)")
+                           } else {
+                               print("Document meme succesfully written")
+                               // reference meme to user
+                               
+                               
+                               let alert = UIAlertController(title: "Noice", message: "Meme uploaded succesfully", preferredStyle: .alert)
+                               alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: {_ in
+                                   self.dismiss(animated: true, completion: nil)
+                               }))
+                               self.present(alert, animated: true, completion: nil)
+                           }
+        }               }
+        
+            taskReference.observe(.progress) { [weak self] (Snapshot) in
+                   guard let pctThere = Snapshot.progress?.fractionCompleted else { return }
+                   print("you are \(pctThere) complete")
+                   self?.progressView.progress = Float(pctThere)
+            }
+                
+            } else {
+            
+               let alert = UIAlertController(title: "Missing Fields", message: "Make sure image is selected and caption and tag fields are filled", preferredStyle: .alert)
+               alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: {_ in
+                   self.dismiss(animated: true, completion: nil)
+               }))
+               self.present(alert, animated: true, completion: nil)
+           }
+                   }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
-}
-}
