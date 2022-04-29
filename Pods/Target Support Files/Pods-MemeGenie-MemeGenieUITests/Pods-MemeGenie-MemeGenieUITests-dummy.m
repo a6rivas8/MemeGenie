d40f63cd19fbcbcd71d303f0f6157940cc2e08a9
Alexander Rivas <32589133+a6rivas8@users.noreply.github.com>
2022-04-29 03:20:56 -0500

    Delete Pods directory

--- Pods/Target Support Files/Pods-MemeGenie-MemeGenieUITests/Pods-MemeGenie-MemeGenieUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MemeGenie_MemeGenieUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MemeGenie_MemeGenieUITests
-@end
--- Pods/Target Support Files/Pods-MemeGenieTests/Pods-MemeGenieTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MemeGenieTests : NSObject
-@end
-@implementation PodsDummy_Pods_MemeGenieTests
-@end
